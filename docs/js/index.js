41b8b82e2013bde84b7b4895dff964e1a47e53eb
William Palin <bill@free.law>
2023-10-13 20:08:10 -0400

    feat(pages): Fix placeholder yml

---
+++ docs/js/index.js
@@ -0,0 +1,16 @@
+
+fetch('data/db.json')
+  .then(response => response.json())
+  .then(data => {
+    // Process the JSON data here
+    console.log(data);
+    $.each(data, function(key, value) {
+        $('#main').append('<p>' + key + ': ' + value + '</p>');
+    });
+
+  })
+  .catch(error => {
+    // Handle any errors during the fetch request
+    console.error('Error fetching the JSON file:', error);
+  });
+
